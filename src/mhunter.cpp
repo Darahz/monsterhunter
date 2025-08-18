62e7af11e414ddbbc4275a0d364d3329504f4e5d
Darahz <darahzmail@gmail.com>
2025-08-18 14:44:08 +0200

    Testing classes for now

--- src/mhunter.cpp
+++ src/mhunter.cpp
@@ -1,7 +1,15 @@
 #include "../include/Window.h"
+#include "../include/Inventory.h"
+#include "../include/Player.h"
+
+#include <unistd.h>
 
 int main(){
     Window window;
+    Player player;
+
+    sleep(2); // Sleep for 2 seconds to see the inventory output before the window opens
+
     if (!window.initialize()) {
         return -1;
     }
