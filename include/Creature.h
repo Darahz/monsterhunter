e49059ee46b68100d15020d315dee44c07557418
Darahz <darahzmail@gmail.com>
2025-08-18 14:43:27 +0200

    feat: add setname function

--- include/Creature.h
+++ include/Creature.h
@@ -1,14 +1,13 @@
 #pragma once
-#include <SFML/Graphics.hpp>
 #include <iostream>
 
-// Class to include all creatures including player and enemies
 class Creature {
 public:
     int getHealth() const { return health; }
     std::string getName() const { return name; }
 
     void setHealth(int newHealth) { health = newHealth; }
+    void setName(const std::string& newName) { name = newName; }
     void hurt(int damage) { health -= damage; }
     void heal(int amount) { health += amount; }
 
