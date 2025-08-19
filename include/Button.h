5fb70e369977f34c2044b9c45de3f4438fb8f874
Darahz <darahzmail@gmail.com>
2025-08-19 09:55:15 +0200

    feat: add Button class with basic functionality and event handling

---
+++ include/Button.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <functional>
+
+class Button {
+public:
+    Button(const std::string& text, const sf::Font& font, unsigned int characterSize);
+    void setPosition(float x, float y);
+    void setSize(float width, float height);
+    void setCallback(std::function<void()> callback);
+    void render(sf::RenderTarget& target);
+    bool isMouseOver(const sf::Vector2f& mousePos) const;
+    bool handleEvent(const sf::Event& event);
+
+private:
+    sf::RectangleShape shape;
+    sf::Text label;
+    std::function<void()> onClick;
+};
---
+++ src/Button.cpp
@@ -0,0 +1,48 @@
+#include "../include/Button.h"
+
+Button::Button(const std::string& text, const sf::Font& font, unsigned int characterSize) {
+    label.setFont(font);
+    label.setString(text);
+    label.setCharacterSize(characterSize);
+    label.setFillColor(sf::Color::White);
+
+    shape.setFillColor(sf::Color::Blue);
+    shape.setSize(sf::Vector2f(200, 50));
+}
+
+void Button::setPosition(float x, float y) {
+    shape.setPosition(x, y);
+    label.setPosition(x + 10, y + 10);
+}
+
+void Button::setSize(float width, float height) {
+    shape.setSize(sf::Vector2f(width, height));
+}
+
+void Button::setCallback(std::function<void()> callback) {
+    onClick = callback;
+}
+
+void Button::render(sf::RenderTarget& target) {
+    target.draw(shape);
+    target.draw(label);
+}
+
+bool Button::isMouseOver(const sf::Vector2f& mousePos) const {
+    return shape.getGlobalBounds().contains(mousePos);
+}
+
+bool Button::handleEvent(const sf::Event& event) {
+    if (event.type == sf::Event::MouseButtonPressed) {
+        if (event.mouseButton.button == sf::Mouse::Left) {
+            sf::Vector2f mousePos(event.mouseButton.x, event.mouseButton.y);
+            if (isMouseOver(mousePos)) {
+                if (onClick) {
+                    onClick();
+                }
+                return true;
+            }
+        }
+    }
+    return false;
+}
